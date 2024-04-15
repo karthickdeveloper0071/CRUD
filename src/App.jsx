4f9d8ec3f91e5e603194e1e90418a8f79e7b3265
karthickdeveloper0071 <karthick123svks@gmail.com>
2024-04-15 12:01:59 +0530

    css complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <div>
+      <div className='container'>
+        <h3>Crud application with React</h3>
+        <div className='input-Search'>
+          <input type="search" />
+          <button className='btn green'>Add Record</button>
+        </div>
         
-      </div>
+          <table className='table'>
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Karthick</td>
+              <td>25</td>
+              <td>Salem</td>
+              <td><button className='btn green'>Edit</button></td>
+               <td><button className='btn red'>Delete</button></td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Karthick</td>
+              <td>25</td>
+              <td>Salem</td>
+              <td><button className='btn green'>Edit</button></td>
+               <td><button className='btn red'>Delete</button></td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Karthick</td>
+              <td>25</td>
+              <td>Salem</td>
+              <td><button className='btn green'>Edit</button></td>
+               <td><button className='btn red'>Delete</button></td>
+            </tr>
+          </tbody>
+          </table>
+        </div>
+     
     
     </>
   )
