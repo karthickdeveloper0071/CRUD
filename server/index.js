de3e3f34f39bb05439dba36a7051650d11abf719
karthickdeveloper0071 <karthick123svks@gmail.com>
2024-04-15 11:15:58 +0530

    modify

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express =require("express");
+const users =require("./sample.json")
 const app =express();
 const port=8000;
-
+//display all users
+app.get("/user,(req,res)")
 app.listen(port,(err)=> {
-    console.log(`App is running in poirt`)
+    console.log(`App is running in port ${port}`)
 })
